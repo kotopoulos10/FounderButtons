75e03f40f12caceb684d3146f9582e06a061c414
Tom Kotopoulos <kotopoulos10@gmail.com>
2019-01-17 16:40:34 -0500

    Final Product

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var founder: UILabel!
+    @IBOutlet weak var coFounder: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showGoogleFounders(_ sender: UIButton) {
+        founder.text = "Larry Page"
+        coFounder.text = "Sergey Brin"
+    }
+    
+    @IBAction func showRentTheRunwayFounders(_ sender: UIButton) {
+        founder.text = "Jennifer Hyman"
+        coFounder.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        founder.text = ""
+        coFounder.text = ""
+    }
+    
 }
 
